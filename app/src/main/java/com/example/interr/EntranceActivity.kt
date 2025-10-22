c89f20c88960841f77aa34e1f943b95316ad45ad
Dizekee <dwzcort@gmail.com>
2025-10-22 20:15:34 +0300

    Registration

---
+++ app/src/main/java/com/example/interr/EntranceActivity.kt
@@ -0,0 +1,35 @@
+package com.example.interr
+
+import android.app.ActivityOptions
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EntranceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_entrance)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val registration = findViewById<TextView>(R.id.registration)
+
+        registration.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+
+            val options = ActivityOptions.makeCustomAnimation(
+                this,
+                android.R.anim.fade_in,
+                android.R.anim.fade_out
+            )
+            startActivity(intent, options.toBundle())
+        }
+    }
+}
---
+++ app/src/main/java/com/example/interr/RegisterActivity.kt
@@ -0,0 +1,38 @@
+package com.example.interr
+
+import android.app.ActivityOptions
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val entrance = findViewById<TextView>(R.id.entrance)
+
+        entrance.setOnClickListener {
+            val intent = Intent(this, EntranceActivity::class.java)
+
+            val options = ActivityOptions.makeCustomAnimation(
+                this,
+                android.R.anim.fade_in,
+                android.R.anim.fade_out
+            )
+            startActivity(intent, options.toBundle())
+        }
+
+
+    }
+}
